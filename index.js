6b9365f6a160f97ff207b7d0e463e00647a77717
tvelic1 <tvelic1@etf.unsa.ba>
2024-08-01 11:45:44 +0200

    add user

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
 const jwt = require("jsonwebtoken");
-const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
