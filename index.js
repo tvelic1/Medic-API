3d2a5a9f9774a649d9655ddd335f595042171ff6
tvelic1 <tvelic1@etf.unsa.ba>
2024-08-03 15:58:38 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
-const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-const { generateAndSetToken, authenticateToken } = require("./jwt");
-const { checkUsername, isDateValid } = require("./helpers");
-const { createHash } = require("crypto");
+const {authenticateToken } = require("./jwt");
+const { checkUsername} = require("./helpers");
+const routes=require("./routes")
 
 app.use(
   cors({
@@ -21,12 +20,6 @@ app.use(
   })
 );
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
 
 app.options(
   "*",
@@ -41,210 +34,15 @@ app.options(
 
 app.set("trust proxy", 1);
 
-app.post("/login", async (req, res) => {
-  let { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).send("Username and password are required");
-  }
-  password=createHash('sha256').update(password).digest('hex')
-
-  try {
-    const checkUserQuery = `
-      SELECT username, role FROM users
-      WHERE username = $1 AND password = $2 AND role = 'admin'
-    `;
-    const values = [username, password];
-    const result = await pool.query(checkUserQuery, values);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      generateAndSetToken(user, res);
-      res.status(200).json({ message: "User authenticated successfully." });
-    } else {
-      res.status(401).send("Invalid username, password, or role.");
-    }
-  } catch (err) {
-    console.error("Error checking user:", err);
-    res.status(500).send("Error checking user");
-  }
-});
-
-app.get("/users", authenticateToken, async (req, res) => {
-  try {
-    const result = await pool.query(
-      "SELECT * FROM users WHERE role != 'admin'"
-    );
-    res.status(200).json(result.rows);
-  } catch (err) {
-    console.error("Error fetching users:", err);
-    res.status(500).send("Error fetching users");
-  }
-});
-app.post("/logout", (req, res) => {
-  res.clearCookie("tokenJwtWeb", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-  });
-  res.status(200).json({ message: "Logout successful" });
-});
-
-app.post("/register", authenticateToken, checkUsername, async (req, res) => {
-  let { username, password, name, orders, image_url, date_of_birth } = req.body;
-  password=createHash('sha256').update(password).digest('hex')
-  if (
-    !username ||
-    !password ||
-    !name ||
-    orders === undefined ||
-    !image_url ||
-    !date_of_birth
-  ) {
-    return res.status(400).send("All fields are required");
-  }
-  if (!isDateValid(date_of_birth)) {
-    return res.status(400).send("Invalid date of birth");
-  }
-
-  try {
-    const insertUserQuery = `
-      INSERT INTO users (username, password, name, orders, image_url, date_of_birth)
-      VALUES ($1, $2, $3, $4, $5, $6) RETURNING *
-    `;
-    const values = [username, password, name, orders, image_url, date_of_birth];
-    const result = await pool.query(insertUserQuery, values);
-    res
-      .status(201)
-      .json({ message: "User registered successfully", user: result.rows[0] });
-  } catch (err) {
-    console.error("Error registering user:", err);
-    res.status(500).send("Error registering user");
-  }
-});
-
-app.get("/users/details/:id", authenticateToken, async (req, res) => {
-  const { id } = req.params;
-  try {
-    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-    if (result.rows.length > 0) {
-      res.status(200).json(result.rows[0]);
-    } else {
-      res.status(404).send("User not found");
-    }
-  } catch (err) {
-    console.error("Error fetching user details:", err);
-    res.status(500).send("Error fetching user details");
-  }
-});
-
-app.put(
-  "/users/details/:id",
-  authenticateToken,
-  checkUsername,
-  async (req, res) => {
-    const { id } = req.params;
-    const { username, name, orders, date_of_birth, image_url } = req.body;
-
-    let fieldsToUpdate = [];
-    let values = [];
-
-    if (username) {
-      fieldsToUpdate.push("username = $" + (values.length + 1));
-      values.push(username);
-    }
-    if (name) {
-      fieldsToUpdate.push("name = $" + (values.length + 1));
-      values.push(name);
-    }
-    if (orders !== undefined) {
-      fieldsToUpdate.push("orders = $" + (values.length + 1));
-      values.push(orders);
-    }
-    if (date_of_birth) {
-      if (!isDateValid(date_of_birth))
-        return res.status(400).send("Invalid date of birth");
-
-      fieldsToUpdate.push("date_of_birth = $" + (values.length + 1));
-      values.push(date_of_birth);
-    }
-    if (image_url) {
-      fieldsToUpdate.push("image_url = $" + (values.length + 1));
-      values.push(image_url);
-    }
-
-    if (values.length === 0) {
-      return res.status(200).json({ message: "No changes" });
-    }
-
-    const updateUserQuery = `
-    UPDATE users
-    SET ${fieldsToUpdate.join(", ")}
-    WHERE id = $${values.length + 1}
-    RETURNING *
-  `;
-    values.push(id);
-
-    try {
-      const result = await pool.query(updateUserQuery, values);
-      if (result.rows.length > 0) {
-        res
-          .status(200)
-          .json({ message: "User updated successfully", user: result.rows[0] });
-      } else {
-        res.status(404).send("User not found");
-      }
-    } catch (err) {
-      console.error("Error updating user:", err);
-      res.status(500).send("Error updating user");
-    }
-  }
-);
-
-app.put("/users/block/:id", authenticateToken, async (req, res) => {
-  const { status } = req.body;
-  const { id } = req.params;
-  try {
-    const updateUserQuery = `
-      UPDATE users
-      SET status = $1
-      WHERE id = $2 RETURNING *
-    `;
-    const values = [status, id];
-    const result = await pool.query(updateUserQuery, values);
-    if (result.rows.length > 0) {
-      res
-        .status(200)
-        .json({ message: "User blocked successfully", user: result.rows[0] });
-    } else {
-      res.status(404).send("User not found");
-    }
-  } catch (err) {
-    console.error("Error updating user:", err);
-    res.status(500).send("Error updating user");
-  }
-});
-
-app.delete("/users/:id", authenticateToken, async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deleteUserQuery = `
-      DELETE FROM users
-      WHERE id = $1
-      RETURNING *
-    `;
-    const result = await pool.query(deleteUserQuery, [id]);
-    if (result.rows.length > 0) {
-      res
-        .status(200)
-        .json({ message: "User deleted successfully", user: result.rows[0] });
-    } else {
-      res.status(404).send("User not found");
-    }
-  } catch (err) {
-    console.error("Error deleting user:", err);
-    res.status(500).send("Error deleting user");
-  }
-});
-
+app.post("/login", routes.login)
+app.get("/users", authenticateToken, routes.getUsers)
+app.post("/logout", routes.logout)
+app.post("/register", authenticateToken, checkUsername, routes.addUser)
+app.get("/users/details/:id", authenticateToken, routes.getUserDetails)
+app.put("/users/details/:id",authenticateToken,checkUsername,routes.updateUserDetails)
+app.put("/users/block/:id", authenticateToken, routes.blockUser)
+app.delete("/users/:id", authenticateToken, routes.deleteUser)
+ 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
---
+++ routes.js
@@ -0,0 +1,221 @@
+const { generateAndSetToken} = require("./jwt");
+const { createHash } = require("crypto");
+const {isDateValid } = require("./helpers");
+
+
+const { Pool } = require("pg");
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  
+
+async function login (req,res){
+    const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send("Username and password are required");
+  }
+  const hashpassword=createHash('sha256').update(password).digest('hex')
+
+  try {
+    const checkUserQuery = `
+      SELECT username, role FROM users
+      WHERE username = $1 AND password = $2 AND role = 'admin'
+    `;
+    const values = [username, hashpassword];
+    const result = await pool.query(checkUserQuery, values);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      generateAndSetToken(user, res);
+      res.status(200).json({ message: "User authenticated successfully." });
+    } else {
+      res.status(401).send("Invalid username, password, or role.");
+    }
+  } catch (err) {
+    console.error("Error checking user:", err);
+    res.status(500).send("Error checking user");
+  }
+}
+
+
+async function getUsers(req,res){
+    try {
+        const result = await pool.query(
+          "SELECT * FROM users WHERE role != 'admin'"
+        );
+        res.status(200).json(result.rows);
+      } catch (err) {
+        console.error("Error fetching users:", err);
+        res.status(500).send("Error fetching users");
+      }
+}
+
+async function logout(req,res){
+    res.clearCookie("tokenJwtWeb", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
+      res.status(200).json({ message: "Logout successful" });
+}
+
+
+async function addUser(req,res){
+    const { username, password, name, orders, image_url, date_of_birth } = req.body;
+    const hashpassword=createHash('sha256').update(password).digest('hex')
+    if (
+      !username ||
+      !password ||
+      !name ||
+      orders === undefined ||
+      !image_url ||
+      !date_of_birth
+    ) {
+      return res.status(400).send("All fields are required");
+    }
+    if (!isDateValid(date_of_birth)) {
+      return res.status(400).send("Invalid date of birth");
+    }
+  
+    try {
+      const insertUserQuery = `
+        INSERT INTO users (username, password, name, orders, image_url, date_of_birth)
+        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *
+      `;
+      const values = [username, hashpassword, name, orders, image_url, date_of_birth];
+      const result = await pool.query(insertUserQuery, values);
+      res
+        .status(201)
+        .json({ message: "User registered successfully", user: result.rows[0] });
+    } catch (err) {
+      console.error("Error registering user:", err);
+      res.status(500).send("Error registering user");
+    }
+}
+
+
+async function getUserDetails(req,res){
+    const { id } = req.params;
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    if (result.rows.length > 0) {
+      res.status(200).json(result.rows[0]);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    console.error("Error fetching user details:", err);
+    res.status(500).send("Error fetching user details");
+  }
+}
+
+async function updateUserDetails(req,res){
+    const { id } = req.params;
+    const { username, name, orders, date_of_birth, image_url } = req.body;
+
+    let fieldsToUpdate = [];
+    let values = [];
+
+    if (username) {
+      fieldsToUpdate.push("username = $" + (values.length + 1));
+      values.push(username);
+    }
+    if (name) {
+      fieldsToUpdate.push("name = $" + (values.length + 1));
+      values.push(name);
+    }
+    if (orders !== undefined) {
+      fieldsToUpdate.push("orders = $" + (values.length + 1));
+      values.push(orders);
+    }
+    if (date_of_birth) {
+      if (!isDateValid(date_of_birth))
+        return res.status(400).send("Invalid date of birth");
+
+      fieldsToUpdate.push("date_of_birth = $" + (values.length + 1));
+      values.push(date_of_birth);
+    }
+    if (image_url) {
+      fieldsToUpdate.push("image_url = $" + (values.length + 1));
+      values.push(image_url);
+    }
+
+    if (values.length === 0) {
+      return res.status(200).json({ message: "No changes" });
+    }
+
+    const updateUserQuery = `
+    UPDATE users
+    SET ${fieldsToUpdate.join(", ")}
+    WHERE id = $${values.length + 1}
+    RETURNING *
+  `;
+    values.push(id);
+
+    try {
+      const result = await pool.query(updateUserQuery, values);
+      if (result.rows.length > 0) {
+        res
+          .status(200)
+          .json({ message: "User updated successfully", user: result.rows[0] });
+      } else {
+        res.status(404).send("User not found");
+      }
+    } catch (err) {
+      console.error("Error updating user:", err);
+      res.status(500).send("Error updating user");
+    }
+}
+
+async function blockUser(req,res){
+    const { status } = req.body;
+    const { id } = req.params;
+    try {
+      const updateUserQuery = `
+        UPDATE users
+        SET status = $1
+        WHERE id = $2 RETURNING *
+      `;
+      const values = [status, id];
+      const result = await pool.query(updateUserQuery, values);
+      if (result.rows.length > 0) {
+        res
+          .status(200)
+          .json({ message: "User blocked successfully", user: result.rows[0] });
+      } else {
+        res.status(404).send("User not found");
+      }
+    } catch (err) {
+      console.error("Error updating user:", err);
+      res.status(500).send("Error updating user");
+    }
+}
+
+async function deleteUser(req,res){
+    const { id } = req.params;
+    try {
+      const deleteUserQuery = `
+        DELETE FROM users
+        WHERE id = $1
+        RETURNING *
+      `;
+      const result = await pool.query(deleteUserQuery, [id]);
+      if (result.rows.length > 0) {
+        res
+          .status(200)
+          .json({ message: "User deleted successfully", user: result.rows[0] });
+      } else {
+        res.status(404).send("User not found");
+      }
+    } catch (err) {
+      console.error("Error deleting user:", err);
+      res.status(500).send("Error deleting user");
+    }
+}
+
+
+
+
+module.exports={login,getUsers,logout,addUser,getUserDetails,updateUserDetails,blockUser,deleteUser,}
